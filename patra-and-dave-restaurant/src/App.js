273ad9ac47d05138d6f0e33a1f397dd8d55be646
Emmanuel Nsubuga <107301618+Cemputus@users.noreply.github.com>
2023-11-10 18:36:19 +0300

    Web-App

--- patra-and-dave-restaurant/src/App.js
+++ patra-and-dave-restaurant/src/App.js
@@ -1,20 +1,31 @@
-// import logo from './logo.svg';
-import './App.css';
-// import ' ./Components/SignUp';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Homepage/Navbar";
+import Homepage from "./components/Homepage/Homepage";
+import Restaurants from "../src/components/Restaurants/Res-list/Restaurants";
+import About from "./components/About/About";
+import Account from "./components/User account/Account";
+import Footer from "./components/Footer/Footer";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/signup";
+
 function App() {
   return (
-    <div className="App">
-         <h1>Hello World!</h1>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/Restaurants" element={<Restaurants />} />
+        <Route path="/Restaurants/:id" element={<Restaurants />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
 
-      
-    </div>
-
-  
- );
+        <Route path="/About" element={<About />} />
+        <Route path="/Account" element={<Account />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  );
 }
 
-
-
 export default App;
-
-
